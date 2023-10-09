2bdc307a4284a2f3a8b1987c72700130bcbec822
yuzzin <77273340+babo0121@users.noreply.github.com>
2023-10-09 15:15:25 +0900

    SwiftUI로 project 변경

---
+++ ToMeetToMe/ToMeetToMe/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ToMeetToMe
+//
+//  Created by 조유진 on 2023/10/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ToMeetToMe/ToMeetToMe/ToMeetToMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToMeetToMeApp.swift
+//  ToMeetToMe
+//
+//  Created by 조유진 on 2023/10/09.
+//
+
+import SwiftUI
+
+@main
+struct ToMeetToMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
