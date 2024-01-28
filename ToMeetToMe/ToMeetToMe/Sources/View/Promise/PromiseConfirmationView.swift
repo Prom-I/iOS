f1d21a00f860ca06dab9cd983cd0453fb9b5ff68
yuzzin0121 <ydbwlsjki60@gmail.com>
2024-01-29 06:52:15 +0900

    약속 확정 화면 - 삭제 버전 구현

--- ToMeetToMe/ToMeetToMe/Sources/View/Promise/PromiseConfirmationView.swift
+++ ToMeetToMe/ToMeetToMe/Sources/View/Promise/PromiseConfirmationView.swift
@@ -15,16 +15,26 @@ struct PromiseConfirmationView: View {
         NavigationView{
             VStack {
                 Spacer()
-                    .frame(height: 20)
+                    .frame(height: 30)
                 DateAndTimeView()
                 PlaceView()
                 ParticipantsView()
+                DeleteFromListButtonView()
                 Spacer()
             }
-            .navigationTitle("그룹")
             .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    HStack {
+                        Text("1차 회의")   // 약속 이름
+                            .font(.system(size: 18))
+                        Image("check")
+                    }
+                }
+            }
             .navigationBarItems(leading: BackButton(btnColor: .black),trailing: Button{} label: {
                 Image(systemName: "ellipsis")
+                    .foregroundColor(.black)
             }
             )
         }
@@ -42,19 +52,8 @@ private struct DateAndTimeView: View {
             }
             
             Spacer()
-            Button {
-
-            } label: {
-                RoundedRectangle(cornerRadius: 6)
-                    .fill(Color.mintColor)
-                    .overlay {
-                        Text("확정")
-                            .font(.system(size: 10))
-                            .bold()
-                            .foregroundColor(.white)
-                    }
-                    .frame(width: 32, height: 20)
-            }
+//            ConfirmationLabelView()
+            FinishLablView()
         }
         .padding(.all, 20)
         .overlay{
@@ -64,6 +63,34 @@ private struct DateAndTimeView: View {
                 }
         .padding(.horizontal, 20)
         .padding(.vertical, 10)
+    }
+}
+
+private struct ConfirmationLabelView: View {
+    fileprivate var body: some View {
+        RoundedRectangle(cornerRadius: 6)
+            .fill(Color.mintColor)
+            .overlay {
+                Text("확정")
+                    .font(.system(size: 10))
+                    .bold()
+                    .foregroundColor(.white)
+            }
+            .frame(width: 32, height: 20)
+    }
+}
+
+private struct FinishLablView: View {
+    fileprivate var body: some View {
+        RoundedRectangle(cornerRadius: 6)
+            .fill(Color("textGray"))
+            .overlay {
+                Text("완료")
+                    .font(.system(size: 10))
+                    .bold()
+                    .foregroundColor(.white)
+            }
+            .frame(width: 32, height: 20)
     }
 }
 
@@ -94,8 +121,12 @@ private struct ParticipantsView: View {
     fileprivate var body: some View {
         HStack {
             VStack(alignment: .leading, spacing: 20) {
-                Text("참여자")
-                    .font(.system(size: 14, weight: .bold))
+                HStack(spacing: 4) {
+                    Text("참여자")
+                    Text("12")  // 참여자수
+                        .foregroundStyle(Color("textGray"))
+                }
+                .font(.system(size: 14, weight: .bold))
                 LazyVGrid(columns: Array(repeating: GridItem(), count: 5), spacing: 10) {
                     ForEach(Friend.frinedArray) { friend in
                         ParticipantCellView(friend: friend)
@@ -123,10 +154,29 @@ private struct ParticipantCellView: View {
     
     fileprivate var body: some View {
         Text(friend.nickname)
-            .font(.system(size: 12))
-            .frame(width: 52, height: 20)
+            .font(.system(size: 11))
+            .frame(width: 52, height: 18)
             .background(Color("lightGray"))
             .cornerRadius(4, corners: .allCorners)
+    }
+}
+
+private struct DeleteFromListButtonView: View {
+    fileprivate var body: some View {
+        Button(action: {}) {
+            HStack {
+                Spacer()
+            Text("목록에서 삭제하기")
+                .foregroundStyle(Color("red"))
+                Spacer()
+            }
+            .frame(height: 46)
+        }
+        .background(Color("lightGray"))
+        .cornerRadius(12, corners: .allCorners)
+        .padding(.horizontal, 28)
+        .padding(.top, 30)
+        
     }
 }
 
